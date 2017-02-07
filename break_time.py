d0468eb197e3c6fc4d676dfa4f92e3b87a68da34
Bhaskar <kbslalith@gmail.com>
2017-02-07 14:05:01 +0530

    Commented on the Break_time

--- break_time.py
+++ break_time.py
@@ -1,14 +1,24 @@
+"""
+time.ctime() => this is used to find the current start time including date and day
+time.sleep(ENTER SECONDS HERE) => This is used to make the program wait for few seconds
+webbrowser.open("Enter URL Here") => This is used to pop open a web browser 
+"""
+
+
 import webbrowser
 import time
 
-start_time = time.ctime()
+def break_time():
+	start_time = time.ctime()
 
-print("This Program is started on "+ start_time)
+	print("This Program is started on "+ start_time)
 
-total_breaks = 3
-wait_time = 2 # Seconds
+	total_breaks = 3
+	wait_time = 2 # Seconds
 
 
-for i in range(0,total_breaks):
-    time.sleep(wait_time)
-    webbrowser.open("https://www.youtube.com/watch?v=ZHDtda9K36c")
+	for i in range(0,total_breaks):
+    		time.sleep(wait_time)
+    		webbrowser.open("https://www.youtube.com/watch?v=ZHDtda9K36c")
+
+break_time()
